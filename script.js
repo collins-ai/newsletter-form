53591a3ca8ec9c76d969bfba4f2502def93ab4f6
collins-ai <collinsosas843@gmail.com>
2024-10-24 19:17:35 -0700

    javascript email validation included

---
+++ script.js
@@ -0,0 +1,15 @@
+const form = document.getElementById(`form`)
+const email = document.getElementById(`email`)
+const errMessage = document.getElementById(`errmessage`)
+const pattern = /^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/
+
+form.addEventListener(`submit`, (e) => {
+    if(!email.value.match(pattern)) {
+        e.preventDefault()
+        errMessage.style.display = `inline-block`
+        email.style.borderColor = `red`
+        email.style.outlineColor = `red`
+        email.style.backgroundColor = `pink`
+        email.style.color = `red`
+    }
+})
